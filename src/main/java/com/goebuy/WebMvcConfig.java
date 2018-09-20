1f957a4b29b1a27d7cef7276505e49621dde8663
GoEBuy <GoEBuy@github.com>
2018-09-21 00:12:43 +0800

    update

--- src/main/java/com/goebuy/WebMvcConfig.java
+++ src/main/java/com/goebuy/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package com.goebuy;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,6 +15,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+	
+	 @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8", true);
+        return filter;
+    }
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
     	
